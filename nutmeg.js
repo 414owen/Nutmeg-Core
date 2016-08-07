23694b9c5314b2b17244848cb8180551461e8351
Owen Shepherd <shephero@tcd.ie>
2016-08-07 14:14:41 +0100

    nutmeg is function format

--- nutmeg.js
+++ nutmeg.js
@@ -15,120 +15,123 @@
   *
   */
 
-var D = document,
-    W = window;
+function nutmeg() {    
+    this.D = document;
+    this.W = window;
+    var nutmeg = {};
 
-function setStyles(el, styles) {
-    styles.forEach(function(style) {
-        Object.assign(el.style, style);
-    });
-}
+    var setStyles = function(el, styles) {
+        styles.forEach(function(style) {
+            Object.assign(el.style, style);
+        });
+    };
 
-function appendChildren(el, children) {
-    children.forEach(function(child) {
-        el.appendChild(child.val);
-    });
-}
+    var appendChildren = function(el, children) {
+        children.forEach(function(child) {
+            el.appendChild(child.val);
+        });
+    };
 
-function setClasses(el, classes) {
-    classes.forEach(function(classname) {
-        el.classList.add(classname);
-    });
-}
+    var setClasses = function(el, classes) {
+        classes.forEach(function(classname) {
+            el.classList.add(classname);
+        });
+    };
 
-function addClickEvent(el, event) {
-    el.onclick = function() {
-        if (el.onclick !== undefined) {
-            el.onclick();
-        }
-        event();
-    }
-}
-
-function elify(el) {
-    function elified() {
-        var scope = this;
-        this.val = el;
-        this.append = function(children) {
-            appendChildren(this.val, children);
-            return scope;
-        };
-        this.onclick = function(todo) {
-            scope.val.onclick = todo;
-            return scope;
-        };
-        this.link = function(url) {
-            scope.onclick(function() {
-                window.location = url;
-            });
-            return scope.style([{cursor: 'pointer'}]);
-        };
-        this.style = function(styles) {
-            setStyles(scope.val, styles);
-            return scope;
-        };
-        this.classes = function(classes) {
-            setClasses(scope.val, classes);
-            return scope;
-        };
-    }
-    return new elified();
-
-}
-
-function get(name) {
-    return elify(D.createElement(name));
-}
-
-function text(text) {
-    return elify(D.createTextNode(text));
-}
-
-var elNames = [
-    'div',
-    'span',
-    'em',
-    'p',
-    'strong',
-    'a',
-    'h1',
-    'h2',
-    'h3',
-    'h4',
-    'h5',
-    'h6',
-    'hr',
-    'ol',
-    'ul',
-    'li',
-    'br',
-    'i'
-];
-
-function bod() {return elify(D.body);}
-
-elNames.forEach(function(elName) {
-    window[elName] = function() {
-        return get(elName);
-    }
-});
-
-function mergeStyle(root) {
-    const styleGroups = {};
-    for (var key in root) {
-        var styles = [];
-        function merge(style) {
-            var deps = style.depends;
-            if (deps !== undefined) {
-                deps.forEach(function(dep) {
-                    merge(root[dep]);
-                });
+    var addClickEvent = function(el, event) {
+        el.onclick = function() {
+            if (el.onclick !== undefined) {
+                el.onclick();
             }
-            styles.push(style);
+            event();
         }
-        merge(root[key])
-        delete styles.depends;
-        styleGroups[key] = styles;
+    };
+
+    nutmeg.elify = function(el) {
+        return new function () {
+            var scope = this;
+            this.val = el;
+            this.append = function(children) {
+                appendChildren(scope.val, children);
+                return scope;
+            };
+            this.onclick = function(todo) {
+                addClickEvent(scope.val, todo);
+                return scope;
+            };
+            this.link = function(url) {
+                addClickEvent(function() {W.location = url;});
+                return scope.style([{cursor: 'pointer'}]);
+            };
+            this.style = function(styles) {
+                setStyles(scope.val, styles);
+                return scope;
+            };
+            this.classes = function(classes) {
+                setClasses(scope.val, classes);
+                return scope;
+            };
+        }
+    };
+
+    nutmeg.text = function(text) {
+        return nutmeg.elify(D.createTextNode(text));
+    };
+
+    var elNames = [
+        'div',
+        'span',
+        'em',
+        'p',
+        'strong',
+        'a',
+        'h1',
+        'h2',
+        'h3',
+        'h4',
+        'h5',
+        'h6',
+        'hr',
+        'ol',
+        'ul',
+        'li',
+        'br',
+        'i'
+    ];
+
+    nutmeg.bod = function() {return elify(D.body);}
+
+    elNames.forEach(function(elName) {
+        nutmeg[elName] = function() {
+            return elify(D.createElement(elName));
+        }
+    });
+
+    nutmeg.mergeStyle = function(root) {
+        const styleGroups = {};
+        for (var key in root) {
+            var styles = [];
+            function merge(style) {
+                var deps = style.depends;
+                if (deps !== undefined) {
+                    deps.forEach(function(dep) {
+                        merge(root[dep]);
+                    });
+                }
+                styles.push(style);
+            }
+            merge(root[key])
+            delete styles.depends;
+            styleGroups[key] = styles;
+        }
+        return styleGroups;
     }
-    return styleGroups;
+
+    nutmeg.global = function() {
+        Object.keys(nutmeg).forEach(function(key) {
+            W[key] = nutmeg[key];
+        });
+    }
+    debugger;
+    return nutmeg;
 }
