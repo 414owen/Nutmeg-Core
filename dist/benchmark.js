4e2d8ba38e4cb3ef9aca3b24cfd89e4addf7d2fd
Owen Shepherd <shephero@tcd.ie>
2016-08-26 13:18:21 +0100

    Extracted examples to their own javascript files, deleted comparison

---
+++ dist/benchmark.js
@@ -0,0 +1,48 @@
+
+window.onload = function() {
+
+    var nut = nutmeg();
+    for (var key in nut) {
+        eval('var ' + key + '=nut[key];');
+    }
+
+    var style = mergeStyle({
+        base: {
+            backgroundColor: '#111'
+        },
+        spaced: {
+            margin: '0.5rem',
+            padding: '0.5rem'
+        },
+        bordered: {
+            borderRadius: '8px'
+        },
+        fib: {
+            depends: ['spaced', 'bordered'],
+            display: 'inline-block',
+            backgroundColor: '#333',
+            color: '#eee'
+        }
+    });
+    
+    var timeTaken = 0;
+    var timesToLoad = 50;
+    for (var i = 0; i < timesToLoad; i++) {
+        var startTime = Date.now();
+        var curr = 1;
+        var prev = 0;
+        body.style(style.base)(
+            Array.apply(null, Array(1500)).map(function (fib, ind) {
+                var oldc = curr, oldp = prev;
+                curr += prev;
+                prev = oldc;
+                return div(oldp).style(style.fib);
+            })
+        );
+        timeTaken += Date.now() - startTime;
+        if (i < timesToLoad - 1) 
+            body().clear();
+    }
+    console.log("Page loaded " + timesToLoad + " times.");
+    console.log("Average load time: " + timeTaken / timesToLoad + " ms.");
+};
---
+++ dist/fibs.js
@@ -0,0 +1,47 @@
+window.onload = function() {
+
+    var nut = nutmeg();
+    for (var key in nut) {
+        eval('var ' + key + '=nut[key];');
+    }
+
+    var fg = '#eee';
+    var bg = '#333';
+
+    var style = mergeStyle({
+        base: {
+            backgroundColor: '#111'
+        },
+        spaced: {
+            margin: '0.5rem',
+            padding: '0.5rem'
+        },
+        bordered: {
+            borderRadius: '8px'
+        },
+        fib: {
+            depends: ['spaced', 'bordered'],
+            display: 'inline-block',
+            backgroundColor: bg,
+            color: fg,
+            transition: 'all 0.2s ease',
+            hover: {
+                color: bg,
+                backgroundColor: fg
+            }
+        }
+    });
+
+
+
+    var curr = 1;
+    var prev = 0;
+    body().style(style.base)(
+        Array.apply(null, Array(1500)).map(function (fib, ind) {
+            var oldc = curr, oldp = prev;
+            curr += prev;
+            prev = oldc;
+            return div(ind + ': ' + oldp).style(style.fib);
+        })
+    );
+};
